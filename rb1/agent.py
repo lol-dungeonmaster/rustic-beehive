949ca74f9c4d69e340264e442bd988d1d92098d4
dungeonmaster <actual@oswind.xyz>
2025-12-04 15:41:54 -0500

    start agent /wo plugins, compaction

--- rb1/agent.py
+++ rb1/agent.py
@@ -1,3 +1,17 @@
+from google.adk.agents import Agent
+from google.adk.apps.app import App, EventsCompactionConfig
+from google.adk.apps.llm_event_summarizer import LlmEventSummarizer
+from google.adk.models.google_llm import Gemini as LLM
+from google.adk.plugins import ReflectAndRetryToolPlugin
+from google.genai import types
+
+retry_config = types.HttpRetryOptions(
+    attempts=5,
+    exp_base=2,
+    initial_delay=3,
+    http_status_codes=[429, 500, 503, 504],
+)
+
 root_agent = Agent(
     model=LLM(
         model="gemini-2.5-flash",
@@ -14,10 +28,5 @@ root_agent = Agent(
 app = App(
     name="agents",
     root_agent=root_agent,
-    plugins=[ReflectAndRetryToolPlugin(max_retries=1)],
-    events_compaction_config=EventsCompactionConfig(
-        summarizer=LlmEventSummarizer(LLM(model="gemini-2.5-flash")),
-        compaction_interval=3,
-        overlap_size=0,
-    ),
+    events_compaction_config=None
 )
